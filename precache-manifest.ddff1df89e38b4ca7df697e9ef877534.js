272662708e40a523d38dedb1d47894bac3fe0e4f
danto133 <danto13397@gmail.com>
2020-02-13 15:15:45 +0700

    Updates

---
+++ precache-manifest.ddff1df89e38b4ca7df697e9ef877534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ee0453630ea6e1a1ecac60ad5c35306",
+    "url": "/first-react-calculator/index.html"
+  },
+  {
+    "revision": "3856d3c6caaeb64aa9f9",
+    "url": "/first-react-calculator/static/css/main.506b18da.chunk.css"
+  },
+  {
+    "revision": "30aa5f71cb42c6c09771",
+    "url": "/first-react-calculator/static/js/2.f1be420e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/first-react-calculator/static/js/2.f1be420e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3856d3c6caaeb64aa9f9",
+    "url": "/first-react-calculator/static/js/main.abc52cad.chunk.js"
+  },
+  {
+    "revision": "6b038fa48f57ca183292",
+    "url": "/first-react-calculator/static/js/runtime-main.9be07481.js"
+  }
+]);
